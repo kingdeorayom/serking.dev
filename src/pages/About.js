637933ccbc32a35a745c9abcde549641232a40c3
Serking de Orayom <kingdeorayom@gmail.com>
2022-09-09 20:39:47 +0800

    change some about info

--- src/pages/About.js
+++ src/pages/About.js
@@ -40,41 +40,41 @@ const About = () => {
 
             <section id='about'>
                 <h1 className="page__title">About</h1>
-                <h2 className="page__content">Hello, I'm Serking. I'm a software developer.</h2>
+                <h2 className="page__content">Hi, I'm Serking. I'm a software developer.</h2>
                 <h2 className="page__content">I earned my bachelor's degree in Information Technology at the University of Perpetual Help System Laguna with an academic honor of distinction: <em>Magna Cum Laude</em>.</h2>
-                <h2 className="page__content">I am currently looking for a job.</h2>
+                <h2 className="page__content">I currently work at <a href='https://padtechcorp.com/' target={"_blank"} rel="noreferrer" className='link__text'>Padtech Industries Corporation.</a></h2>
 
                 <h1 className="page__subtitle">Outside of work</h1>
                 <h2 className="page__content">I spend most of my free time reading books, listening to music, playing video games or watching movies, TV series or anime.</h2>
 
                 <section className="gallery">
-                    <img src={bob_ong_56} className='img-fluid about__gallery__image' alt="About" />
-                    <img src={rubber_soul} className='img-fluid about__gallery__image' alt="About" />
-                    <img src={witcher} className='img-fluid about__gallery__image' alt="About" />
-                    <img src={conan} className='img-fluid about__gallery__image image__disappear' alt="About" />
+                    <img src={bob_ong_56} className='img-fluid about__gallery__image' alt="Bob Ong's 56" />
+                    <img src={rubber_soul} className='img-fluid about__gallery__image' alt="The Beatles Rubber Soul" />
+                    <img src={witcher} className='img-fluid about__gallery__image' alt="The Witcher 3: Wild Hunt" />
+                    <img src={conan} className='img-fluid about__gallery__image image__disappear' alt="Detective Conan" />
                 </section>
 
                 <h2 className="page__content">If time permits, I also hang out with my friends and family.</h2>
 
                 <section className="gallery">
 
-                    <a href={image_1} data-fancybox="gallery"><img src={image_1} className='img-fluid about__gallery__image' alt="About" /></a>
-                    <a href={image_2} data-fancybox="gallery"><img src={image_2} className='img-fluid about__gallery__image' alt="About" /></a>
-                    <a href={image_3} data-fancybox="gallery"><img src={image_3} className='img-fluid about__gallery__image' alt="About" /></a>
-                    <a href={image_4} data-fancybox="gallery"><img src={image_4} className='img-fluid about__gallery__image' alt="About" /></a>
-                    <a href={image_5} data-fancybox="gallery"><img src={image_5} className='img-fluid about__gallery__image' alt="About" /></a>
-                    <a href={image_6} data-fancybox="gallery"><img src={image_6} className='img-fluid about__gallery__image' alt="About" /></a>
-                    <a href={image_7} data-fancybox="gallery"><img src={image_7} className='img-fluid about__gallery__image' alt="About" /></a>
-                    <a href={image_8} data-fancybox="gallery"><img src={image_8} className='img-fluid about__gallery__image' alt="About" /></a>
-                    <a href={image_9} data-fancybox="gallery"><img src={image_9} className='img-fluid about__gallery__image' alt="About" /></a>
-                    <a href={image_10} data-fancybox="gallery"><img src={image_10} className='img-fluid about__gallery__image' alt="About" /></a>
-                    <a href={image_11} data-fancybox="gallery"><img src={image_11} className='img-fluid about__gallery__image' alt="About" /></a>
-                    <a href={image_12} data-fancybox="gallery"><img src={image_12} className='img-fluid about__gallery__image' alt="About" /></a>
+                    <a href={image_1} data-fancybox="gallery"><img src={image_1} className='img-fluid about__gallery__image' alt="About Gallery" /></a>
+                    <a href={image_2} data-fancybox="gallery"><img src={image_2} className='img-fluid about__gallery__image' alt="About Gallery" /></a>
+                    <a href={image_3} data-fancybox="gallery"><img src={image_3} className='img-fluid about__gallery__image' alt="About Gallery" /></a>
+                    <a href={image_4} data-fancybox="gallery"><img src={image_4} className='img-fluid about__gallery__image' alt="About Gallery" /></a>
+                    <a href={image_5} data-fancybox="gallery"><img src={image_5} className='img-fluid about__gallery__image' alt="About Gallery" /></a>
+                    <a href={image_6} data-fancybox="gallery"><img src={image_6} className='img-fluid about__gallery__image' alt="About Gallery" /></a>
+                    <a href={image_7} data-fancybox="gallery"><img src={image_7} className='img-fluid about__gallery__image' alt="About Gallery" /></a>
+                    <a href={image_8} data-fancybox="gallery"><img src={image_8} className='img-fluid about__gallery__image' alt="About Gallery" /></a>
+                    <a href={image_9} data-fancybox="gallery"><img src={image_9} className='img-fluid about__gallery__image' alt="About Gallery" /></a>
+                    <a href={image_10} data-fancybox="gallery"><img src={image_10} className='img-fluid about__gallery__image' alt="About Gallery" /></a>
+                    <a href={image_11} data-fancybox="gallery"><img src={image_11} className='img-fluid about__gallery__image' alt="About Gallery" /></a>
+                    <a href={image_12} data-fancybox="gallery"><img src={image_12} className='img-fluid about__gallery__image' alt="About Gallery" /></a>
 
                 </section>
 
                 <h1 className="page__subtitle">Contact</h1>
-                <h2 className="page__content">Contact me at kingdeorayom@gmail.com or <a href='https://twitter.com/kingdeorayom' target={"_blank"} rel="noreferrer" className="link__text__twitter">Twitter</a>.</h2>
+                <h2 className="page__content">Contact me at kingdeorayom@gmail.com or <a href='https://twitter.com/kingdeorayom' target={"_blank"} rel="noreferrer" className="link__text">Twitter</a>.</h2>
 
             </section>
 
