12e55b4e8a768f8eef9d4282d91a032c84774328
serking <kingdeorayom@gmail.com>
2023-12-27 13:12:33 +0800

    update: project

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -18,7 +18,7 @@ function LinkIcon(props) {
 
 export default function Projects() {
   const headline = "Things I’ve made trying to put my mark on the tech industry."
-  const intro = "I’ve worked on tons of little projects over the years, but these are some of the ones that I’m most proud of. If you see something that piques your interest, check it out via the link below. While you're there, feel free to contribute any ideas that would make it better. Remember: some are Open Source."
+  const intro = "I’ve worked on tons of little projects over the years, but these are some of the ones that I’m most proud of. If you see something that piques your interest, check it out via the link below. While you're there, feel free to contribute any ideas that would make it better. Note: Project logos are placeholder. I have yet to fix it."
 
   return (
     <>
@@ -50,14 +50,14 @@ export default function Projects() {
         >
           {projects.map((project) => (
             <Card as="li" key={project.name}>
-              {/* <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+              <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
                 <Image
                   src={project.logo}
                   alt=""
                   className="h-8 w-8"
                   unoptimized
                 />
-              </div> */}
+              </div>
               <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
                 <Card.Link href={project.link.href} target="_blank">{project.name}</Card.Link>
               </h2>
