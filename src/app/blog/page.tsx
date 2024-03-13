537e76af0108b88dde34fce7206683626faa8b5a
serking <kingdeorayom@gmail.com>
2024-03-13 22:13:39 +0800

    update: create initial pages

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Blog | Serking de Orayom",
+    description: "Web and mobile app developer",
+};
+
+export default function Blog() {
+    return (
+        <main className="flex-auto min-w-0 flex flex-col">
+            <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+                Coming soon! &#x1F51C;
+            </h1>
+        </main>
+    );
+}
