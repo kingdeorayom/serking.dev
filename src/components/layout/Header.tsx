b386452778d91bb3a93db845a9558269088142dd
serking <kingdeorayom@gmail.com>
2024-03-13 22:07:28 +0800

    update: const nav links, installed react-icons, etc

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,40 +2,21 @@ import Link from "next/link";
 // import ThemeToggler from "../ui/ThemeToggler";
 import MenuToggler from "../ui/MenuToggler";
 import Avatar from "../ui/Avatar";
+import { navLinks } from "@/lib/consts";
 
 const Header = () => {
-    const navItems = {
-        "/": {
-            name: "home",
-        },
-        "/work": {
-            name: "work",
-        },
-        "/about": {
-            name: "about",
-        },
-        "/blog": {
-            name: "blog",
-        },
-        "/guestbook": {
-            name: "guestbook",
-        },
-    };
-
     return (
         <header className="mb-12 flex items-center justify-between tracking-tight">
             <nav className="hidden md:flex flex-row space-x-6 pr-10">
-                {Object.entries(navItems).map(([path, { name }]) => {
-                    return (
-                        <Link
-                            key={path}
-                            href={path}
-                            className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1"
-                        >
-                            {name}
-                        </Link>
-                    );
-                })}
+                {navLinks.map((item) => (
+                    <Link
+                        key={item.path}
+                        href={item.path}
+                        className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1"
+                    >
+                        {item.label}
+                    </Link>
+                ))}
             </nav>
             <Avatar />
             <div className="flex gap-x-2.5">
