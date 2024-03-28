12339be8e5e54ee7b8c0c130ce4753ab11dc06d4
serking <kingdeorayom@gmail.com>
2024-03-28 20:43:34 +0800

    fix: auth options file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,5 @@
-import { AuthOptions } from "next-auth";
+import { authOptions } from "@/utils/authOptions";
 import NextAuth from "next-auth/next";
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
-
-export const authOptions: AuthOptions = {
-    providers: [
-        GitHub({
-            clientId: process.env.GITHUB_OAUTH_CLIENT_ID as string,
-            clientSecret: process.env.GITHUB_OAUTH_CLIENT_SECRET as string,
-        }),
-        Google({
-            clientId: process.env.GOOGLE_OAUTH_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET as string,
-        }),
-    ],
-};
 
 export const handler = NextAuth(authOptions);
 
