3c44b1dd841944ba04cd26c47cf2a678c32d14de
serking <kingdeorayom@gmail.com>
2024-12-25 18:31:40 +0800

    update: remove social media links from homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,33 +1,10 @@
 import CopyToClipboard from "@/app/CopyToClipboard";
 import Link from "next/link";
-import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
-import { FiArrowUpRight } from "react-icons/fi";
 
 export default function HomePage() {
-    const SOCIALS = [
-        {
-            description: "LinkedIn",
-            url: "https://www.linkedin.com/in/kingdeorayom/",
-            text: "Serking de Orayom",
-            icon: <FaLinkedin size={18} color="#0077b5" className="" />,
-        },
-        {
-            description: "GitHub",
-            url: "https://github.com/kingdeorayom",
-            text: "@kingdeorayom",
-            icon: <FaGithub size={18} className="" />,
-        },
-        {
-            description: "Facebook",
-            url: "https://facebook.com/kingdeorayom",
-            text: "Serking de Orayom",
-            icon: <FaFacebook size={18} color="#1877F2" className="" />,
-        },
-    ];
-
     return (
         <>
-            <section className="flex flex-col gap-y-6">
+            <section className="mb-8 flex flex-col gap-y-6">
                 <p>
                     I currently work as a frontend developer at{" "}
                     <Link
@@ -63,41 +40,6 @@ export default function HomePage() {
                     </Link>
                     .
                 </p>
-            </section>
-            <section className="my-6 flex flex-col gap-y-6">
-                <div className="my-2 flex flex-wrap">
-                    {SOCIALS.map((item, index) => {
-                        return (
-                            <div
-                                className="group mx-1 my-1 flex w-fit"
-                                key={index}
-                            >
-                                <Link
-                                    href={item.url}
-                                    target="_blank"
-                                    className="w-full"
-                                >
-                                    <div className="flex items-center justify-between rounded-lg border border-neutral-200 p-3 hover:bg-slate-50 dark:border-neutral-700 dark:bg-neutral-900 dark:hover:bg-neutral-800">
-                                        <div className="flex items-center space-x-2">
-                                            <div>{item.icon}</div>
-                                            <div className="flex flex-col">
-                                                <p className="line-clamp-1 text-[13px] font-medium text-neutral-900 dark:text-neutral-100">
-                                                    {item.description}
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div className="transform text-neutral-700 transition-transform duration-300 group-hover:-rotate-12 dark:text-neutral-300">
-                                            <FiArrowUpRight
-                                                size={16}
-                                                className="ms-2"
-                                            />
-                                        </div>
-                                    </div>
-                                </Link>
-                            </div>
-                        );
-                    })}
-                </div>
             </section>
             <CopyToClipboard />
         </>
