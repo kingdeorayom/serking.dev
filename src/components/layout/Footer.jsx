98a3467240595116b88200688cb4f30cf4fc3267
serking <kingdeorayom@gmail.com>
2025-04-03 10:24:20 +0800

    update: change footer heading to paragraph for improved semantics

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="text-xs text-center text-gray-500 dark:text-gray-200 space-y-2">
-            <h6>{`© ${new Date().getFullYear()} Serking de Orayom`}</h6>
+            <p>{`© ${new Date().getFullYear()} Serking de Orayom`}</p>
             <p>This is both a portfolio and a personal website.</p>
         </footer>
     );
