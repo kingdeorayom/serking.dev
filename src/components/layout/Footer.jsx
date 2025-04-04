46f6f8ae077a2045c0132ad394d25e059419e803
serking <kingdeorayom@gmail.com>
2025-04-04 10:00:05 +0800

    update: comment out portfolio description in Footer component

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="text-xs text-center text-gray-500 dark:text-gray-200 space-y-2">
             <p>{`© ${new Date().getFullYear()} Serking de Orayom`}</p>
-            <p>This is both a portfolio and a personal website.</p>
+            {/* <p>This is both a portfolio and a personal website.</p> */}
         </footer>
     );
 };
