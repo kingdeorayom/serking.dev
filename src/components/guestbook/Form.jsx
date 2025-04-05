57e2ad41883bd8e6b2288ed0e9a89830db67f389
serking <kingdeorayom@gmail.com>
2025-04-05 22:56:29 +0800

    update: standardize button hover styles for consistency across components

--- src/components/guestbook/Form.jsx
+++ src/components/guestbook/Form.jsx
@@ -41,7 +41,7 @@ const Form = () => {
                         <button
                             type="submit"
                             // disabled={pending}
-                            className="flex h-10 cursor-pointer items-center rounded-md border border-gray-200 bg-slate-50 px-3 py-2 text-sm leading-4 text-neutral-900 hover:bg-slate-100 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-100 dark:hover:hover:bg-neutral-800"
+                            className="flex h-10 cursor-pointer items-center rounded-md border border-gray-200 bg-slate-50 px-3 py-2 text-sm leading-4 text-neutral-900 hover:bg-slate-100 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100 dark:hover:bg-neutral-800"
                         >
                             Submit
                         </button>
