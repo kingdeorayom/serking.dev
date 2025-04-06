f4ba8bd0861a210b7bf0e5266758fb7bc8080a90
serking <kingdeorayom@gmail.com>
2025-04-06 21:13:58 +0800

    fix: update gallery images for consistency and remove unused imports

--- src/components/ui/Gallery.jsx
+++ src/components/ui/Gallery.jsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
 
-import about12 from "../../../public/images/about/about12.jpg";
+import about1 from "../../../public/images/about/about1.jpg";
+import about2 from "../../../public/images/about/about2.jpg";
 import about3 from "../../../public/images/about/about3.jpg";
-import about13 from "../../../public/images/about/about13.jpg";
-// import about4 from "../../../public/images/about/about4.jpg";
-import about9 from "../../../public/images/about/about9.jpg";
+import about4 from "../../../public/images/about/about4.jpg";
 import about5 from "../../../public/images/about/about5.jpg";
-// import about6 from "../../../public/images/about/about6.jpg";
-import about7 from "../../../public/images/about/about7.jpg";
+import about6 from "../../../public/images/about/about6.jpg";
 import Link from "next/link";
 
 const Gallery = () => {
@@ -16,11 +14,11 @@ const Gallery = () => {
             <div className="-m-1 flex flex-wrap md:-m-2">
                 <div className="flex w-1/2 flex-wrap">
                     <div className="w-1/2 cursor-pointer p-1 hover:opacity-90 md:p-2">
-                        <Link href={about12.src} target="_blank">
+                        <Link href={about1.src} target="_blank">
                             <Image
                                 alt="gallery"
                                 className="block h-full w-full rounded-2xl object-cover object-center"
-                                src={about12}
+                                src={about1}
                             />
                         </Link>
                     </div>
@@ -34,17 +32,6 @@ const Gallery = () => {
                         </Link>
                     </div>
                     <div className="w-full cursor-pointer p-1 hover:opacity-90 md:p-2">
-                        <Link href={about13.src} target="_blank">
-                            <Image
-                                alt="gallery"
-                                className="block h-full w-full rounded-2xl object-cover object-center"
-                                src={about13}
-                            />
-                        </Link>
-                    </div>
-                </div>
-                <div className="flex w-1/2 flex-wrap">
-                    <div className="w-full cursor-pointer p-1 hover:opacity-90 md:p-2">
                         <Link href={about3.src} target="_blank">
                             <Image
                                 alt="gallery"
@@ -53,21 +40,32 @@ const Gallery = () => {
                             />
                         </Link>
                     </div>
-                    <div className="w-1/2 cursor-pointer p-1 hover:opacity-90 md:p-2">
-                        <Link href={about7.src} target="_blank">
+                </div>
+                <div className="flex w-1/2 flex-wrap">
+                    <div className="w-full cursor-pointer p-1 hover:opacity-90 md:p-2">
+                        <Link href={about2.src} target="_blank">
                             <Image
                                 alt="gallery"
                                 className="block h-full w-full rounded-2xl object-cover object-center"
-                                src={about7}
+                                src={about2}
                             />
                         </Link>
                     </div>
                     <div className="w-1/2 cursor-pointer p-1 hover:opacity-90 md:p-2">
-                        <Link href={about9.src} target="_blank">
+                        <Link href={about6.src} target="_blank">
                             <Image
                                 alt="gallery"
                                 className="block h-full w-full rounded-2xl object-cover object-center"
-                                src={about9}
+                                src={about6}
+                            />
+                        </Link>
+                    </div>
+                    <div className="w-1/2 cursor-pointer p-1 hover:opacity-90 md:p-2">
+                        <Link href={about4.src} target="_blank">
+                            <Image
+                                alt="gallery"
+                                className="block h-full w-full rounded-2xl object-cover object-center"
+                                src={about4}
                             />
                         </Link>
                     </div>
