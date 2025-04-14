233e5efe33f3baa16ac751a21964acdf954ee58e
serking <kingdeorayom@gmail.com>
2025-04-14 23:12:48 +0800

    fix: correct class name for DownloadCV button styling

--- src/components/ui/DownloadCV.jsx
+++ src/components/ui/DownloadCV.jsx
@@ -12,7 +12,7 @@ const DownloadCV = () => {
     return (
         <button
             onClick={handleClick}
-            className="flex h-10 w-fit cursor-pointer items-center rounded-md border border-gray-200 bg-slate-50 px-3 py-2 text-sm leading-4 font-medium text-neutral-900 hover:bg-slate-100 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100 dark:hover:bg-neutral-800"
+            className="flex h-10 w-fit cursor-pointer items-center rounded-md border border-gray-200 bg-slate-50 px-3 py-2 text-sm leading-4 font-medium text-neutral-900 hover:bg-slate-100 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-100 hover:dark:bg-neutral-800"
         >
             Download CV
             <MdArrowDownward className="ms-2 h-4 w-4 fill-neutral-900 dark:fill-neutral-100" />
